c0728509a002e47e61a3fb44d74e2f5b720518ea
kenlsm <kenlsm@u.nus.edu>
2016-10-23 01:01:59 -0700

    Add playerhome

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,56 +1,89 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Messages from './Messages'
+import { Button, Panel, ListGroup, ListGroupItem, Col, Image, Row } from 'react-bootstrap';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.nextScreen = this.nextScreen(this);
+  }
+
+  nextScreen() {
+    this.props.dispatch({type: 'GAME_START'});
+  }
+
   render () {
     return (
       <div className='container-fluid'>
-        <Messages messages={this.props.messages} />
-        <div className='row'>
-          <div className='col-sm-4'>
-            <div className='panel'>
-              <div className='panel-body'>
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                </p>
-                <a href='#' role='button' className='btn btn-default'>View details</a>
-              </div>
-            </div>
-          </div>
-          <div className='col-sm-4'>
-            <div className='panel'>
-              <div className='panel-body'>
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                </p>
-                <a href='#' role='button' className='btn btn-default'>View details</a>
-              </div>
-            </div>
-          </div>
-          <div className='col-sm-4'>
-            <div className='panel'>
-              <div className='panel-body'>
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                </p>
-                <a href='#' role='button' className='btn btn-default'>View details</a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <Row>
+        <Col md={2} /> {/* HACKY FORMATING */}
+        <Col md={2}>
+          <Panel>
+            <ListGroup fill>
+              <ListGroupItem>
+                <Image responsive src={'/images/chinese-woman-player.png'} />
+              </ListGroupItem>
+              <ListGroupItem className='face_name'>Ken Lee</ListGroupItem>
+            </ListGroup>
+          </Panel>
+        </Col>
+        <Col md={2}>
+          <Panel>
+            <ListGroup fill>
+              <ListGroupItem>
+                <Image responsive src={'/images/geek-guy-player.png'} />
+              </ListGroupItem>
+              <ListGroupItem className='face_name'>Linh</ListGroupItem>
+            </ListGroup>
+          </Panel>
+        </Col>
+        <Col md={2}>
+          <Panel>
+            <ListGroup fill>
+              <ListGroupItem>
+                <Image responsive src={'/images/indian-girl-player.png'} />
+              </ListGroupItem>
+              <ListGroupItem className='face_name'>Jing Yu</ListGroupItem>
+            </ListGroup>
+          </Panel>
+        </Col>
+        <Col md={2}>
+          <Panel>
+            <ListGroup fill>
+              <ListGroupItem>
+                <Image responsive src={'/images/rapper-guy-player.png'} />
+              </ListGroupItem>
+              <ListGroupItem className='face_name'>Ken Hua</ListGroupItem>
+            </ListGroup>
+          </Panel>
+        </Col>
+        </Row>
+        <Row>
+      <Col mdOffset={5} md={2}>
+        <Panel>
+          <ListGroup fill>
+            <ListGroupItem>
+              <Image responsive src={'/images/redhead-guy-player.png'} />
+            </ListGroupItem>
+            <ListGroupItem className='face_name'>You</ListGroupItem>
+            <ListGroupItem className='face_name'>
+              <Button bsStyle="success" style={{width: "80%"}} onClick={this.nextScreen} >
+                Join
+              </Button>
+            </ListGroupItem>
+          </ListGroup>
+        </Panel>
+      </Col>
+      </Row>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    messages: state.messages
-  }
-}
+const mapStateToProps = (state) => ({
+    messages: state.messages,
+    gameStart: state.gameStart,
+})
 
 export default connect(mapStateToProps)(Home)
